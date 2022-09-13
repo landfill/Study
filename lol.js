70e2476a6b7e97f28e79d985cd02b88ab99c6a16
surroundyou <surroundyou@outlook.com>
2022-09-14 00:26:01 +0900

    Object 삭제할것

---
+++ lol.js
@@ -0,0 +1,17 @@
+
+   /* 
+    var nunu = {
+        q : 'consume',
+        w : 'snowball'
+    }
+    var garen = {
+        q : 'strike',
+        w : 'courage'
+    }*/
+
+    function machine(){
+        this.q = 'consume';
+        this.w = 'snowball';
+    }
+
+    var nunu = new machine();
